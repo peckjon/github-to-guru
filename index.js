1bf0f4d1b51c5cde2f212d06e08a91a34c9678ad
Jon Peck <peckjon@gmail.com>
2020-04-20 15:12:12 -0700

    Better logging

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-const axios = require('axios');
+const axios = require(`axios`);
 const core = require(`@actions/core`);
 const github = require(`@actions/github`);
 
 async function getCollection(auth, collectionId) {
-  console.log('collection: ', collectionId)
-  return axios.get('https://api.getguru.com/api/v1/collections/'+collectionId, {auth: auth})
+  console.log(`collection: ${collectionId}`)
+  return axios.get(`https://api.getguru.com/api/v1/collections/`+collectionId, {auth: auth})
 }
 
 async function createCard(auth, collectionId, title, content) {
-  console.log('collection: ', collectionId)
+  console.log(`creating card in ${collectionId}: ${title}`)
   let headers = {
     auth: auth,
-    'content-type': 'application/json'
+    'content-type': `application/json`
   };
   let data = {
     preferredPhrase: title,
     content: content,
     htmlContent: false,
-    collection: {'id': collectionId}
+    collection: {id: collectionId}
   }
-  return axios.post('https://api.getguru.com/api/v1/facts/extended', data, headers)
+  return axios.post(`https://api.getguru.com/api/v1/facts/extended`, data, headers)
 }
 
 try {
@@ -30,33 +30,35 @@ try {
     password: process.env.GURU_USER_TOKEN
   };
   let nCreated = 0;
+  core.setOutput(`created`, `${nCreated}`);
   getCollection(
     auth,
     process.env.GURU_COLLECTION_ID
   ).then(response => {
-    console.log('collectionType: ', response.data.collectionType);
-    console.log('slug: ', response.data.slug);
-    console.log('cards: ', response.data.cards);
-    console.log('publicCards: ', response.data.publicCards);
-    if(response.data.collectionType=='EXTERNAL') {
-      core.setFailed('Workflow for Synched Collections has not yet been implemented');
+    console.log(`collectionType:`, response.data.collectionType);
+    console.log(`slug:`, response.data.slug);
+    console.log(`cards:`, response.data.cards);
+    console.log(`publicCards:`, response.data.publicCards);
+    if(response.data.collectionType==`EXTERNAL`) {
+      core.setFailed(`Workflow for Synched Collections has not yet been implemented`);
     } else {
       createCard(
         auth,
         process.env.GURU_COLLECTION_ID,
-        'Test from GitHub-to-Guru action',
-        'This is a test post from [GitHub to Guru](https://github.com/peckjon/github-to-guru)'
+        `Test from GitHub-to-Guru action`,
+        `This is a test post from [GitHub to Guru](https://github.com/peckjon/github-to-guru)`
       ).then(response => {
         nCreated += 1;
-        console.log(response);
+        console.log(`  id: ${response.data.id}`);
+        console.log(`  slug: ${response.data.slug}`);
+        core.setOutput(`created`, `${nCreated}`);
       }).catch(error => {
-        core.setFailed('Unable to get collection info: '+error.message);
+        core.setFailed(`Unable to get collection info: ${error.message}`);
       });
     }
   }).catch(error => {
-    core.setFailed('Unable to get collection info: '+error.message);
+    core.setFailed(`Unable to get collection info: ${error.message}`);
   });
-  core.setOutput(`created`, `${nCreated}`);
 } catch (error) {
   core.setFailed(error.message);
 }
