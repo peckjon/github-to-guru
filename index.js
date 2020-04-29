29776ff485d60f96d81331a51815966ad34ae035
Jon Peck <peckjon@gmail.com>
2020-04-28 17:43:27 -0700

    Refactor for clarity

--- index.js
+++ index.js
@@ -11,18 +11,18 @@ async function getCollection(auth, collectionId) {
   return axios.get(`https://api.getguru.com/api/v1/collections/`+collectionId, {auth: auth})
 }
 
-async function syncCollection(tmpdirname, auth, collectionId) {
+async function apiSendSynchedCollection(sourceDir, auth, collectionId) {
   let options = {};
-  options.cwd=tmpdirname;
+  options.cwd=sourceDir;
   await exec.exec(`zip`, [`-r`,`guru_collection.zip`,`./`], options);
   if (process.env.DEBUG) {
-    console.log(`DEBUG mode: not deploying ${tmpdirname}/guru_collection.zip to https://api.getguru.com/app/contentsyncupload?collectionId=${collectionId}`);
+    console.log(`DEBUG mode: not deploying ${sourceDir}/guru_collection.zip to https://api.getguru.com/app/contentsyncupload?collectionId=${collectionId}`);
   } else {
-    await exec.exec(`curl -u ${auth.username}:${auth.password} https://api.getguru.com/app/contentsyncupload?collectionId=${collectionId} -F "file=@${tmpdirname}/guru_collection.zip" -D -`);
+    await exec.exec(`curl -u ${auth.username}:${auth.password} https://api.getguru.com/app/contentsyncupload?collectionId=${collectionId} -F "file=@${sourceDir}/guru_collection.zip" -D -`);
   }
 }
 
-async function createCard(auth, collectionId, title, content) {
+async function apiSendStandardCard(auth, collectionId, title, content) {
   console.log(`creating card in ${collectionId}: ${title}`)
   let headers = {
     auth: auth,
@@ -37,21 +37,78 @@ async function createCard(auth, collectionId, title, content) {
   return axios.post(`https://api.getguru.com/api/v1/facts/extended`, data, headers)
 }
 
-function copyCollectionData(tmpdir) {
+function copyCollectionData(targetDir) {
   if (process.env.GURU_COLLECTION_YAML) {
-    console.log(`Copying ${process.env.GURU_COLLECTION_YAML} to ${tmpdir.name}/collection.yaml`);
-    fs.copySync(process.env.GURU_COLLECTION_YAML, `${tmpdir.name}/collection.yaml`);
+    console.log(`Copying ${process.env.GURU_COLLECTION_YAML} to ${targetDir}/collection.yaml`);
+    fs.copySync(process.env.GURU_COLLECTION_YAML, `${targetDir}/collection.yaml`);
   }
   else {
-    console.log(fs.readFileSync(tmpdir.name, "utf8"));
-    console.log(`Writing to ${tmpdir.name}/collection.yaml:`);
-    fs.writeFileSync(`${tmpdir.name}/collection.yaml`, `--- ~\n`);
+    console.log(`Writing to ${targetDir}/collection.yaml:`);
+    fs.writeFileSync(`${targetDir}/collection.yaml`, `--- ~\n`);
+  }
+}
+
+function processExternalCollection(auth) {
+  var tmpdir = tmp.dirSync();
+  console.log('tmpdir: ', tmpdir.name);
+  var tmpCardsDir = `${tmpdir.name}/cards`;
+  fs.mkdirSync(tmpCardsDir);
+  copyCollectionData(tmpdir.name);
+  if(process.env.GURU_CARD_DIR) {
+    fs.copySync(process.env.GURU_CARD_DIR, tmpCardsDir);
+  } else {
+    let cardConfigs = yaml.parse(fs.readFileSync(process.env.GURU_CARD_YAML, 'utf8'));
+    console.log(cardConfigs)
+    for (let cardFilename in cardConfigs) try {
+      let tmpfileBase=cardFilename.replace(/\.md$/gi,'').replace(/[^a-zA-Z0-9]/gi, '_');
+      while(fs.existsSync(`${tmpCardsDir}/${tmpfileBase}.yaml`)) {
+        tmpfileBase+=`_`;
+      }
+      console.log(`Creating ${tmpCardsDir}/${tmpfileBase}.yaml`);
+      fs.copySync(cardFilename,`${tmpCardsDir}/${tmpfileBase}.md`);
+      let cardConfig = cardConfigs[cardFilename];
+      if (!cardConfig.ExternalId) {
+        cardConfig.ExternalId = `${process.env.GITHUB_REPOSITORY}/${cardFilename}`
+      }
+      if (!cardConfig.ExternalUrl) {
+        cardConfig.ExternalUrl = `https://github.com/${process.env.GITHUB_REPOSITORY}/blob/master/${cardFilename}`
+      }
+      var cardYaml=yaml.stringify(cardConfig);
+      fs.writeFileSync(`${tmpCardsDir}/${tmpfileBase}.yaml`, cardYaml);
+    } catch (error) {
+      core.setFailed(`Unable to prepare tempfiles: ${error.message}`);
+      return;
+    }
+  }
+  apiSendSynchedCollection(tmpdir.name,auth,process.env.GURU_COLLECTION_ID).catch(error => {
+    core.setFailed(`Unable to sync collection: ${error.message}`);
+  });
+}
+
+function processStandardCollection(auth) {
+  if(process.env.GURU_CARD_DIR) {
+    core.setFailed("GURU_CARD_DIR is only supported for EXTERNAL collections: https://developer.getguru.com/docs/guru-sync-manual-api");
+    return;
+  } else {
+    let cardConfigs = yaml.parse(fs.readFileSync(process.env.GURU_CARD_YAML, 'utf8'));
+    for (let cardFilename in cardConfigs) try {
+      apiSendStandardCard(
+        auth,
+        process.env.GURU_COLLECTION_ID,
+        cardConfigs[cardFilename].Title,
+        fs.readFileSync(cardFilename, "utf8")
+      ).then(response => {
+        console.log(`Created card for ${cardFilename}`);
+      }).catch(error => {
+        core.setFailed(`Unable to create card for ${cardFilename}: ${error.message}`);
+      });
+    } catch (error) {
+      core.setFailed(`Unable to prepare card: ${error.message}`);
+    }
   }
 }
 
 try {
-  let context = github.context;
-  // console.log(context);
   let auth = {
     username: process.env.GURU_USER_EMAIL,
     password: process.env.GURU_USER_TOKEN
@@ -60,67 +117,16 @@ try {
     auth,
     process.env.GURU_COLLECTION_ID
   ).then(response => {
-    console.log(`Found ${response.data.collectionType} collection at https://app.getguru.com/collections/${response.data.slug}`);
-    console.log(`${response.data.cards} cards, ${response.data.publicCards} publc`);
-    if(process.env.GURU_CARD_DIR) {
-      if(response.data.collectionType==`EXTERNAL`) {
-        var tmpdir = tmp.dirSync();
-        console.log('TmpDir: ', tmpdir.name);
-        copyCollectionData(tmpdir);
-        fs.mkdirSync(`${tmpdir.name}/cards`);
-        fs.copySync(process.env.GURU_CARD_DIR, `${tmpdir.name}/cards`);
-        syncCollection(tmpdir.name,auth,process.env.GURU_COLLECTION_ID).catch(error => {
-          core.setFailed(`Unable to sync collection: ${error.message}`);
-        });
-      } else {
-        core.setFailed("GURU_CARD_DIR is only supported for EXTERNAL collections: https://developer.getguru.com/docs/guru-sync-manual-api");
-      }
-    } else if(process.env.GURU_CARD_YAML) {
-      let cardConfig = fs.readFileSync(process.env.GURU_CARD_YAML, 'utf8');
-      let cards = yaml.parse(cardConfig);
-      console.log(cards)
-      if(response.data.collectionType==`EXTERNAL`) {
-        var tmpdir = tmp.dirSync();
-        console.log('TmpDir: ', tmpdir.name);
-        copyCollectionData(tmpdir);
-        fs.mkdirSync(`${tmpdir.name}/cards`);
-        for (let filename in cards) try {
-          console.log(cards[filename].Title);
-          let tmpfilename=filename.replace(/\.md$/gi,'').replace(/[^a-zA-Z0-9]/gi, '_');
-          fs.copySync(filename,`${tmpdir.name}/cards/${tmpfilename}.md`);
-          var cardYaml=yaml.stringify({
-            'Title': `${cards[filename].Title}`,
-            'ExternalId': `${process.env.GITHUB_REPOSITORY}/${filename}`,
-            'ExternalUrl': `https://github.com/${process.env.GITHUB_REPOSITORY}/blob/master/${filename}`
-          })
-          console.log(`Writing to ${tmpdir.name}/cards/${tmpfilename}.yaml:`);
-          console.log(cardYaml);
-          fs.writeFileSync(`${tmpdir.name}/cards/${tmpfilename}.yaml`, cardYaml);
-        } catch (error) {
-          core.setFailed(`Unable to prepare tempfiles: ${error.message}`);
-        }
-        syncCollection(tmpdir.name,auth,process.env.GURU_COLLECTION_ID).catch(error => {
-          core.setFailed(`Unable to sync collection: ${error.message}`);
-        });
-      } else {
-        for (let filename in cards) try {
-          console.log(cards[filename].Title);
-          createCard(
-            auth,
-            process.env.GURU_COLLECTION_ID,
-            cards[filename].Title,
-            fs.readFileSync(filename, "utf8")
-          ).then(response => {
-            console.log(`Created card for ${filename}`);
-          }).catch(error => {
-            core.setFailed(`Unable to create card for ${filename}: ${error.message}`);
-          });
-        } catch (error) {
-          core.setFailed(`Unable to prepare card: ${error.message}`);
-        }
-      }
-    } else {
+    console.log(`Found ${response.data.collectionType} collection at https://app.getguru.com/collections/${response.data.slug} with ${response.data.cards} cards (${response.data.publicCards} publc)`);
+    let isExternalCollection = response.data.collectionType == `EXTERNAL`;
+    if(!(process.env.GURU_CARD_DIR||process.env.GURU_CARD_YAML)) {
       core.setFailed(`Specify either GURU_CARD_DIR or GURU_CARD_YAML`);
+      return;
+    }
+    if(isExternalCollection) {
+      processExternalCollection(auth);
+    } else {
+      processStandardCollection(auth);
     }
   }).catch(error => {
     core.setFailed(`Unable to get collection info: ${error.message}`);
