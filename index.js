f518383a62ecf163de741f5908d5c595551107a4
Jon Peck <peckjon@gmail.com>
2021-03-29 17:03:06 -0700

    [Feature request] Pass jobId as variable #6

--- index.js
+++ index.js
@@ -19,7 +19,11 @@ async function apiSendSynchedCollection(sourceDir, auth, collectionId) {
   if (process.env.DEBUG) {
     console.log(`DEBUG mode: not deploying ${sourceDir}/guru_collection.zip to https://api.getguru.com/app/contentsyncupload?collectionId=${collectionId}`);
   } else {
-    fs.writeFile('runguru.sh',`export GURU_SYNC_RESPONSE=$(curl -u ${auth.username}:${auth.password} https://api.getguru.com/app/contentsyncupload?collectionId=${collectionId} -F "file=@${sourceDir}/guru_collection.zip" -D - ) && echo "GURU_SYNC_RESPONSE=$GURU_SYNC_RESPONSE" >> $GITHUB_ENV`)
+    let curl_cmd = `curl -u ${auth.username}:${auth.password} https://api.getguru.com/app/contentsyncupload?collectionId=${collectionId} -F "file=@${sourceDir}/guru_collection.zip" -D -`
+    fs.writeFile('runguru.sh',`#!/bin/sh
+    GURU_SYNC_RESPONSE=$(`+curl_cmd+`)
+    echo "GURU_SYNC_RESPONSE=$GURU_SYNC_RESPONSE" >> $GITHUB_ENV
+    `)
     await exec.exec(`chmod u+x runguru.sh`);
     await exec.exec(`sh runguru.sh`);
   }
